3be1a3da4d3572e7bb24895e076646c563962c55
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-30 18:09:24 -0300

    React_5_Counter Parte 1.2: Add button

--- counter/src/Counter1.js
+++ counter/src/Counter1.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter1() {
-  const counter = 0;
+  const [counter, setCounter] = useState(0);
+  const add = () => {
+    setCounter(counter + 1);
+  };
 
   return (
     <div className="Counter1">
       <div>
         <h2>Counter: {counter}</h2>
+        <button onClick={add}>+</button>
       </div>
     </div>
   );
