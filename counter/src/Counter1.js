924029a9097a44f79899c118f43186b863d966a6
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-30 18:06:48 -0300

    React_5_Counter Parte 1.1: Counter

---
+++ counter/src/Counter1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Counter1() {
+  const counter = 0;
+
+  return (
+    <div className="Counter1">
+      <div>
+        <h2>Counter: {counter}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Counter1;
--- counter/src/index.js
+++ counter/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Counter1 from "./Counter1";
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <App />
+    <Counter1 />
   </StrictMode>
 );
 
